c9005359eb0d9ab7f618ed8c4e2438c8747fe486
ABHI <abhi202864@gmail.com>
2024-06-12 22:05:49 +0530

    start js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const inpBox = document.querySelector(".input-box");
+const searchBtn = document.querySelector("#searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temprature = document.querySelector(".temperature");
+const discription = document.querySelector(".discription");
+const humidity_text = document.querySelector("#humidity-text");
+const wind_speed = document.querySelector("#wind-speed");
